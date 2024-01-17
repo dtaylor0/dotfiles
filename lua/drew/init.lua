cbf01592ecd1b25df13024484281bce869d255f4
Drew Taylor <drewtaylorm@gmail.com>
2024-01-17 11:51:55 -0500

    nvim config

---
+++ lua/drew/init.lua
@@ -0,0 +1,4 @@
+require("drew.remap")
+require("drew.lazy")
+
+vim.cmd [[set number]]
---
+++ lua/drew/lazy.lua
@@ -0,0 +1,50 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup(
+{
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			transparent = true,
+		},
+	},
+	{ 'rose-pine/neovim', name = 'rose-pine', opts = {
+		transparent = true,
+	}
+},
+{
+	'nvim-telescope/telescope.nvim', tag = '0.1.5',
+	-- or                              , branch = '0.1.x',
+	dependencies = { 'nvim-lua/plenary.nvim' }
+},
+"tpope/vim-fugitive",
+{
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+},
+"williamboman/mason.nvim",
+"williamboman/mason-lspconfig.nvim",
+"neovim/nvim-lspconfig",
+'neovim/nvim-lspconfig',
+'hrsh7th/cmp-nvim-lsp',
+'hrsh7th/cmp-buffer',
+'hrsh7th/cmp-path',
+'hrsh7th/cmp-cmdline',
+'hrsh7th/nvim-cmp',
+'L3MON4D3/LuaSnip',
+'saadparwaiz1/cmp_luasnip',
+}
+)
