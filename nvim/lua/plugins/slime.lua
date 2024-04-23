66299ef96ba8ebbe4143bcf4bb7aa3896839e085
Drew Taylor <drewtaylorm@gmail.com>
2024-04-23 12:40:37 -0400

    order of operations

--- nvim/lua/plugins/slime.lua
+++ nvim/lua/plugins/slime.lua
@@ -9,6 +9,29 @@ return {
 	{
 		"jpalardy/vim-slime",
 		init = function()
+			local function slime_use_tmux()
+				vim.g.slime_target = "tmux"
+				vim.g.slime_bracketed_paste = 1
+				vim.g.slime_python_ipython = 0
+				vim.g.slime_no_mappings = 1
+				vim.g.slime_default_config = { socket_name = "default", target_pane = ":.2" }
+				vim.g.slime_dont_ask_default = 1
+			end
+
+			local function slime_use_neovim()
+				vim.g.slime_target = "neovim"
+				vim.g.slime_bracketed_paste = 1
+				vim.g.slime_python_ipython = 1
+				vim.g.slime_no_mappings = 1
+				vim.g.slime_get_jobid = slime_get_jobid
+				vim.g.slime_config = {}
+				-- vim.g.slime_default_config = nil
+				-- vim.g.slime_dont_ask_default = 0
+			end
+
+			slime_use_neovim()
+			-- slime_use_tmux()
+			-- }}
 			vim.g.slime_last_channel = { nil }
 			-- will use `# %%` to define cells
 			vim.g.slime_cell_delimiter = "\\s*#\\s*%%"
@@ -20,6 +43,35 @@ return {
 
 			local function prev_cell()
 				vim.fn.search(vim.g.slime_cell_delimiter, "b")
+			end
+			local slime_get_jobid = function()
+				local buffers = vim.api.nvim_list_bufs()
+				local terminal_buffers = { "Select terminal:\tjobid\tname" }
+				local name = ""
+				local jid = 1
+				local chosen_terminal = 1
+
+				for _, buf in ipairs(buffers) do
+					if vim.bo[buf].buftype == "terminal" then
+						jid = vim.api.nvim_buf_get_var(buf, "terminal_job_id")
+						name = vim.api.nvim_buf_get_name(buf)
+						table.insert(terminal_buffers, jid .. "\t" .. name)
+					end
+				end
+
+				-- if there is more than one terminal, ask which one to use
+				if #terminal_buffers > 2 then
+					chosen_terminal = vim.fn.inputlist(terminal_buffers)
+				else
+					chosen_terminal = jid
+				end
+
+				if chosen_terminal then
+					print("\n[slime] jobid chosen: ", chosen_terminal)
+					return chosen_terminal
+				else
+					print("No terminal found")
+				end
 			end
 
 			vim.keymap.set("n", "<leader>e", vim.cmd.SlimeSend, { noremap = true, desc = "send line to term" })
@@ -54,59 +106,6 @@ return {
 				vim.cmd("bo vs term://ipython")
 				vim.cmd("wincmd h")
 			end)
-
-			local slime_get_jobid = function()
-				local buffers = vim.api.nvim_list_bufs()
-				local terminal_buffers = { "Select terminal:\tjobid\tname" }
-				local name = ""
-				local jid = 1
-				local chosen_terminal = 1
-
-				for _, buf in ipairs(buffers) do
-					if vim.bo[buf].buftype == "terminal" then
-						jid = vim.api.nvim_buf_get_var(buf, "terminal_job_id")
-						name = vim.api.nvim_buf_get_name(buf)
-						table.insert(terminal_buffers, jid .. "\t" .. name)
-					end
-				end
-
-				-- if there is more than one terminal, ask which one to use
-				if #terminal_buffers > 2 then
-					chosen_terminal = vim.fn.inputlist(terminal_buffers)
-				else
-					chosen_terminal = jid
-				end
-
-				if chosen_terminal then
-					print("\n[slime] jobid chosen: ", chosen_terminal)
-					return chosen_terminal
-				else
-					print("No terminal found")
-				end
-			end
-
-			local function slime_use_tmux()
-				vim.g.slime_target = "tmux"
-				vim.g.slime_bracketed_paste = 1
-				vim.g.slime_python_ipython = 0
-				vim.g.slime_no_mappings = 1
-				vim.g.slime_default_config = { socket_name = "default", target_pane = ":.2" }
-				vim.g.slime_dont_ask_default = 1
-			end
-
-			local function slime_use_neovim()
-				vim.g.slime_target = "neovim"
-				vim.g.slime_bracketed_paste = 1
-				vim.g.slime_python_ipython = 1
-				vim.g.slime_no_mappings = 1
-				vim.g.slime_get_jobid = slime_get_jobid
-				-- vim.g.slime_default_config = nil
-				-- vim.g.slime_dont_ask_default = 0
-			end
-
-			slime_use_neovim()
-			-- slime_use_tmux()
-			-- }}
 		end,
 	},
 }
