e1d0f3ea0abcd0c3a0faecea070e1731b1ba1fe5
Drew Taylor <drewtaylorm@gmail.com>
2024-08-23 14:02:39 -0400

    remerging nvim into dotfiles

--- nvim/ftplugin/python.lua
+++
@@ -1,1 +0,0 @@
-vim.keymap.set("n", "<leader>rr", ":!python3 %<CR>")
--- nvim/lua/plugins/folke.lua
+++
@@ -1,5 +0,0 @@
-return {
-	{ "folke/neodev.nvim", opts = {} },
-	"folke/which-key.nvim",
-	{ "folke/neoconf.nvim", cmd = "Neoconf" },
-}
