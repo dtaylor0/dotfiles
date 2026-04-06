51beb5220e8d912e06b741b8eab82f19bb9daa05
Drew Taylor <drewtaylorm@gmail.com>
2026-04-06 17:35:50 -0400

    enhance nvim: improve editing, lsp, keymaps; configure yabai workspaces
    - Add mini plugins (pairs, surround, statusline) and which-key
    - Add undotree for persistent undo visualization
    - Expand LSP support: ocamllsp, gopls, pyright with language formatters
    - Add treesitter textobjects for better navigation
    - New keymaps: split navigation, lsp operations, diagnostic browsing
    - Configure yabai workspace rules: Ghostty→space 1, Chrome→space 2

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,6 +6,7 @@ vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.signcolumn = "yes"
 vim.o.swapfile = false
+vim.o.undofile = true
 vim.g.mapleader = " "
 vim.o.winborder = "rounded"
 -- vim.cmd("set completeopt=menuone,noselect,fuzzy,nosort")
@@ -24,6 +25,9 @@ vim.pack.add({
 	{ src = "https://github.com/nvim-mini/mini.completion" },
 	{ src = "https://github.com/nvim-mini/mini.snippets" },
 	{ src = "https://github.com/nvim-mini/mini.extra" },
+	{ src = "https://github.com/nvim-mini/mini.pairs" },
+	{ src = "https://github.com/nvim-mini/mini.surround" },
+	{ src = "https://github.com/nvim-mini/mini.statusline" },
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
 	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
@@ -37,6 +41,12 @@ vim.pack.add({
 	},
 	{ src = "https://github.com/stevearc/conform.nvim" },
 	{ src = "https://github.com/nvim-lua/plenary.nvim" },
+	{ src = "https://github.com/folke/which-key.nvim" },
+	{ src = "https://github.com/mbbill/undotree" },
+	{
+		src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
+		start = true,
+	},
 })
 
 require("oil").setup({
@@ -54,6 +64,12 @@ require("mini.visits").setup()
 require("mini.extra").setup()
 require("mini.snippets").setup()
 require("mini.completion").setup()
+require("mini.pairs").setup()
+require("mini.surround").setup()
+require("mini.statusline").setup()
+require("gitsigns").setup()
+require("which-key").setup()
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 local conform = require("conform")
 conform.setup({
 	formatters_by_ft = {
@@ -65,6 +81,10 @@ conform.setup({
 		html = { "prettierd", "prettier", stop_after_first = true },
 		htmlangular = { "prettierd", "prettier", stop_after_first = true },
 		xml = { "prettierd", "prettier", stop_after_first = true },
+		lua = { "stylua" },
+		python = { "ruff_format" },
+		elixir = { "mix" },
+		ocaml = { "ocamlformat" },
 	},
 	default_format_opts = {
 		lsp_format = "fallback",
@@ -77,6 +97,8 @@ require("mason-lspconfig").setup({
 		"ts_ls",
 		"elixirls",
 		"rust_analyzer",
+		"gopls",
+		"pyright",
 	},
 })
 
@@ -92,9 +114,44 @@ vim.lsp.config("sourcekit", {
 })
 vim.lsp.enable("sourcekit")
 
+vim.lsp.config("ocamllsp", {
+	cmd = { "ocamllsp" },
+	filetypes = { "ocaml", "ocaml.menhir", "ocaml.interface", "ocaml.ocamllex", "reason", "dune" },
+	root_markers = { "dune-project", ".git", "dune-workspace", "*.opam" },
+})
+vim.lsp.enable("ocamllsp")
+
 require("nvim-treesitter").setup({
-	ensure_installed = { "elixir" },
+	ensure_installed = {
+		"elixir", "lua", "typescript", "javascript", "rust",
+		"swift", "ocaml", "go", "python", "java", "json",
+		"html", "css", "bash", "toml", "yaml", "markdown",
+	},
 	highlight = { enable = true },
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true,
+			keymaps = {
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@class.outer",
+				["ic"] = "@class.inner",
+				["ia"] = "@parameter.inner",
+				["aa"] = "@parameter.outer",
+			},
+		},
+		move = {
+			enable = true,
+			set_jumps = true,
+			goto_next_start = {
+				["]m"] = "@function.outer",
+			},
+			goto_previous_start = {
+				["[m"] = "@function.outer",
+			},
+		},
+	},
 })
 vim.api.nvim_create_autocmd("FileType", {
 	callback = function(args)
@@ -147,7 +204,19 @@ vim.keymap.set("n", "<leader>sg", ":Pick grep_live<CR>")
 vim.keymap.set("n", "<leader>s.", ":Pick visit_paths<CR>")
 vim.keymap.set("n", "<leader>h", ":Pick help<CR>")
 vim.keymap.set("n", "<leader>f", conform.format)
+vim.keymap.set("n", "<leader>sd", ":Pick diagnostic<CR>")
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+vim.keymap.set("n", "gr", vim.lsp.buf.references)
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 vim.keymap.set("n", "-", ":Oil<CR>")
+
+-- Split navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
